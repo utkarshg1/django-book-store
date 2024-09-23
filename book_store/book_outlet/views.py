f2354491081489a72d3244a4ff4b7b56888a1921
utkarshg1 <gaikwadujg@gmail.com>
2024-09-24 03:53:10 +0530

    aggregating and sorting

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
+from django.db.models import Avg
 
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-rating")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
     return render(request, "book_outlet/index.html", {
-        "books": books
+        "books": books,
+        "num_books": num_books,
+        "avg_rating": avg_rating
     })
 
 
