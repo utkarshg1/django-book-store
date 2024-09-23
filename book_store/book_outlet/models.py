7a076a3638689d38aa164acfcfeda1c4f12050de
utkarshg1 <gaikwadujg@gmail.com>
2024-09-24 03:41:16 +0530

    added slug

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Book(models.Model):
@@ -9,9 +10,14 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=50)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True)
 
     def get_absolute_url(self):
-        return reverse("model_detail", kwargs={"pk": self.pk})
+        return reverse("book-detail", args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return f"{self.title} : {self.rating}"
--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
-from django.http import Http404
 
 
 def index(request):
@@ -10,8 +9,8 @@ def index(request):
     })
 
 
-def book_detail(request, id):
-    book = get_object_or_404(Book, pk=id)
+def book_detail(request, slug):
+    book = get_object_or_404(Book, slug=slug)
     return render(request, "book_outlet/book_detail.html", {
         "book_title": book.title,
         "author": book.author,
