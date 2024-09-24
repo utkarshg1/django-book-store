5589ef23d48a0824ae1485822c46ef01f093ef29
utkarshg1 <gaikwadujg@gmail.com>
2024-09-24 13:01:38 +0530

    Admin functionality added

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -10,7 +10,8 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=50)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(default="", blank=True,
+                            editable=False, null=False, db_index=True)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
