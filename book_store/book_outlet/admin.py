d4b84f8161ca8f3fb151a69347477363aa6a2ec7
utkarshg1 <gaikwadujg@gmail.com>
2024-09-24 14:20:54 +0530

    Relations in django:

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -4,6 +4,8 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title", )}
+    list_filter = ("rating", "author")
+    list_display = ("title", "author")
 
 
 admin.site.register(Book, BookAdmin)
